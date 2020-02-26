af3da6cf1acecbd0d7cf8d812c8867a130dac7f3
Armando <jarmando.chacon36@gmail.com>
2020-02-26 11:58:28 -0500

    created observers of the product data to load the data to the recyclerView

--- app/src/main/java/com/example/test_asesoftware/activity/ProductsActivity.kt
+++ app/src/main/java/com/example/test_asesoftware/activity/ProductsActivity.kt
@@ -2,13 +2,29 @@ package com.example.test_asesoftware.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.test_asesoftware.R
+import com.example.test_asesoftware.viewModel.ViewModelProducts
 
 class ProductsActivity : AppCompatActivity() {
+
+    private lateinit var viewModelProducts: ViewModelProducts
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViewModel()
+    }
+
+    private fun initViewModel(){
+        viewModelProducts = ViewModelProvider(this).get(ViewModelProducts::class.java)
+    }
+
+    private fun createObservers(){
+        viewModelProducts.products.observe(this, Observer {
+
+        })
     }
 
 }
